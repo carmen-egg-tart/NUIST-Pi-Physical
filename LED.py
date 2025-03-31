2559a211cc6797b8ee11aec1aa5a352244cc3c49
Yuchen Zhang <33946014522qq.com>
2025-03-31 05:06:31 +0100

    LED lamp program1.

--- LED.py
+++ LED.py
@@ -1,16 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
+# Set GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# Set GPIO 18 as output mode
 GPIO.setup(18, GPIO.OUT)
 
+# Turn on the LED
 print("LED on")
 GPIO.output(18, GPIO.HIGH)
 time.sleep(1)
 
+# Turn off the LED
 print("LED off")
 GPIO.output(18, GPIO.LOW)
 
+# Clean up GPIO settings
 GPIO.cleanup()
